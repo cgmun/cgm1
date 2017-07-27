a3867d55f7ccf3be8dda22dc518a69608c050419
root <root@CA3DAQANOMAD07.local>
2017-07-27 21:14:24 +0000

    convert install.sh to ansible playbook and calls playbook now.

--- setup/ansible_env.sh
+++
@@ -1,2 +0,0 @@
-
-ANSIBLE_LIBRARY="/usr/local/lib/python2.7/dist-packages/clc_ansible_module:$ANSIBLE_LIBRARY"
---
+++ setup/myenv.sh
@@ -0,0 +1,2 @@
+
+ANSIBLE_LIBRARY="/usr/local/lib/python2.7/dist-packages:$ANSIBLE_LIBRARY"
