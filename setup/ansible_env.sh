0b37993a54fcbb1200785c342d332ac4ebb2539e
root <root@CA3DAQANOMAD07.local>
2017-07-27 18:27:50 +0000

    Initial upload ansible server scripts

---
+++ setup/ansible_env.sh
@@ -0,0 +1,2 @@
+
+ANSIBLE_LIBRARY="/usr/local/lib/python2.7/dist-packages/clc_ansible_module:$ANSIBLE_LIBRARY"
---
+++ setup/install.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+export oldDir=$PWD
+apt update
+apt upgrade -y
+apt autoremove -y
+apt install software-properties-common -y
+apt-add-repository ppa:ansible/ansiblei -y -u
+#apt update
+#apt autoremove -y
+
+apt-get install `cat listpackages.txt`
+pip install pip --upgrade 
+pip install -r ansible-server-requirements.txt
+
+
+git clone https://github.com/CenturyLinkCloud/clc-python-sdk.git
+cd clc-python-sdk/src
+pip install .
+cd $oldDir
+
+
+git clone https://github.com/CenturyLinkCloud/clc-ansible-module.git
+cd clc-ansible-module
+pip install -r requirements.txt
+cp clc-ansible-module/
+
+# apt-get install $(grep -vE "^\s*#" filename  | tr "\n" " ")
+# xargs -a <(awk '! /^ *(#|$)/' "$packagelist") -r -- sudo apt-get install
+
+
