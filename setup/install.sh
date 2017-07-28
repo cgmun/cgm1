adab3c8bcef88fe64d122bf336a32c50c6aa3adc
root <root@CA3DAQANOMAD07.local>
2017-07-28 11:24:59 +0000

    cleanup

--- setup/install.sh
+++ setup/install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Do just enough to install git and ansible
+# script does a git clone of cgm1 repo and run install.yml playbook.
+
 export oldDir=$PWD
 apt update
 apt upgrade -y
@@ -12,25 +15,4 @@ repo=cgm1
 git clone https://github.com/cgmun/$repo.git
 cd $repo/setup
 ansible-playbook install.yml
-
-
-#apt-get install `cat listpackages.txt`
-#pip install pip --upgrade 
-#pip install -r ansible-server-requirements.txt
-
-
-#git clone https://github.com/CenturyLinkCloud/clc-python-sdk.git
-#cd clc-python-sdk/src
-#pip install .
-#cd $oldDir
-
-
-#git clone https://github.com/CenturyLinkCloud/clc-ansible-module.git
-#cd clc-ansible-module
-#pip install -r requirements.txt
-#cp clc-ansible-module/
-
-# apt-get install $(grep -vE "^\s*#" filename  | tr "\n" " ")
-# xargs -a <(awk '! /^ *(#|$)/' "$packagelist") -r -- sudo apt-get install
-
 
