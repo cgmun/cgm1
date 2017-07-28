533295f0671cd5ccd1df9b4bdac2846d8be2d0a3
root <root@CA3DAQANOMAD07.local>
2017-07-28 14:17:28 +0000

    updating install.sh

--- setup/install.sh
+++ setup/install.sh
@@ -4,20 +4,24 @@
 # script does a git clone of cgm1 repo and run install.yml playbook.
 
 export oldDir=$PWD
+echo "[CGM] apt update running" 
 apt update
-
+echo "[CGM] apt upgrade running"
 yes '' | apt upgrade -y 
 
+echo "[CGM] apt upgrade complete!!!!!!"
+echo "[CGM] apt auto remove running"
 apt autoremove -y
-
+echo "[CGM] apt install  software-properties-common"
 apt install software-properties-common -y
-
+echo "[CGM] apt add repo"
 apt-add-repository ppa:ansible/ansible -y -u
-
+echo "[CGM] apt install ansibe"
 apt install -y git ansible
-
+echo "[CGM] git cgm1"
 repo=cgm1
 git clone https://github.com/cgmun/$repo.git
 cd $repo/setup
+echo "[CGM] run install.yml playbook"
 ansible-playbook install.yml
 
