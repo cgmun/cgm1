1915d7c89642368a1c9a3aceec34ca9365a22275
root <root@CA3DAQANOMAD07.local>
2017-07-28 14:39:04 +0000

    added ansible.cfg

--- setup/files/myenv.sh
+++ setup/files/myenv.sh
@@ -1,2 +1,4 @@
 
-ANSIBLE_LIBRARY="/usr/local/lib/python2.7/dist-packages:$ANSIBLE_LIBRARY"
+export ANSIBLE_LIBRARY="/usr/local/lib/python2.7/dist-packages:$ANSIBLE_LIBRARY"
+export ANSIBLE_HOST_KEY_CHECKING=False
+
