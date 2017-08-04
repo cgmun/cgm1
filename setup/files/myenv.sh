a92db5713ac4118b6f8577b9ee54d3442a02add5
root <root@CA3DAQADEV01.local>
2017-08-04 20:19:50 +0000

    myenv.sh

--- setup/files/myenv.sh
+++ setup/files/myenv.sh
@@ -1,4 +1,4 @@
 
-export ANSIBLE_LIBRARY="/usr/local/lib/python2.7/dist-packages/clc_ansible_module/:$ANSIBLE_LIBRARY"
+#export ANSIBLE_LIBRARY="/usr/local/lib/python2.7/dist-packages/clc_ansible_module/:$ANSIBLE_LIBRARY"
 export ANSIBLE_HOST_KEY_CHECKING=False
 
